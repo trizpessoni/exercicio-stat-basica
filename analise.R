b3605dc4e05f0be49c2697eb043207d534652d31
trizpessoni <beatriz.pessoni@usp.br>
2026-03-24 17:01:45 -0300

    Adiciona cálculo de soma

--- analise.R
+++ analise.R
@@ -1,4 +1,13 @@
-x <- 1:10
-mean(x)
-# 
+###install.packages("usethis")
 
+###usethis::create_github_token()
+
+###install.packages("gitcreds")
+###gitcreds::gitcreds_set()
+
+###usethis::git_sitrep()
+
+x <- 1:10 
+mean(x)
+sum (x)
+# 
